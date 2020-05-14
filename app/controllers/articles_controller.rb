dfd6c5a1d1c8ec527d208848288945b6ce2979c2
stellaywong <stellayinwong@gmail.com>
2020-05-13 21:10:14 -0400

    Views, buttons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,9 +14,34 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         
-        @article.save
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'        # gives @article object back to new, instead of sending a new request
+        end
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        # binding.pry
+        redirect_to articles_path   #why a path now?
+    end
 
     private
     def article_params
