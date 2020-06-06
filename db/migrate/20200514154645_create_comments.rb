e0e8bc6a3d5f3979402d9f5f614ca82c1e0c5c14
stellaywong <stellayinwong@gmail.com>
2020-06-05 21:57:38 -0400

    Add partials

--- db/migrate/20200514154645_create_comments.rb
+++ db/migrate/20200514154645_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.string :commenter
       t.text :body
-      t.references :article, null: false, foreign_key: true  
+      t.references :article, null: false, foreign_key: true  #does this automatically create an association with the resource it lists?
 
       t.timestamps
     end
